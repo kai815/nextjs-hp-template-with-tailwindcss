4b8d18301ccfc953122f4fc3f485cdca3daa4755
kai815 <h.kaizuka815@gmail.com>
2021-01-04 08:57:39 +0900

    add whitebutton component

--- components/atoms/Button/NavyButton.stories.tsx
+++ components/atoms/Button/NavyButton.stories.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from "react";
 import NavyButton from "./NavyButton";
+import WhiteButton from "./WhiteButton";
 
 export default {
   title: "Button",
 };
 
 export const Navy: FC = () => <NavyButton text="ネイビー" />;
+export const White: FC = () => <WhiteButton text="ホワイト" />;
---
+++ components/atoms/Button/WhiteButton.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+
+type WhiteButton = {
+  text: string;
+};
+
+const WhiteButton: FC<WhiteButton> = (props) => {
+  return (
+    <button className="white theme-navy-text theme-navy-border p-2 border-radius-30">
+      {props.text}
+    </button>
+  );
+};
+
+export default WhiteButton;
