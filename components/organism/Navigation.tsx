e8e02f02aebedcfcb279bb27c5a3d713e20c5cd7
kai815 <h.kaizuka815@gmail.com>
2021-01-18 09:53:50 +0900

    add navigation padding

--- components/organism/Navigation.tsx
+++ components/organism/Navigation.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 
 const Navigation: FC = () => {
   return (
-    <div className="theme-green-background">
+    <div className="theme-green-background px-2">
       <p className="text-white">テスト1</p>
       <p className="text-white">テスト2</p>
       <p className="text-white">テスト3</p>
