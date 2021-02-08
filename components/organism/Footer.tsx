098da6edb09f1df831337d46cfe2fd7f755e101d
kai815 <h.kaizuka815@gmail.com>
2021-02-08 22:39:25 +0900

    modify footer

--- components/organism/Footer.tsx
+++ components/organism/Footer.tsx
@@ -4,19 +4,10 @@ import WhiteButton from "../atoms/Button/WhiteButton";
 const Footer: FC = () => {
   return (
     <nav className="theme-navy-background">
-      <h2 className="text-white">車のあいみつ</h2>
-      <ul>
-        <li>
-          <p className="text-white">TOP</p>
-        </li>
-        <li>
-          <p className="text-white">サービス</p>
-        </li>
-        <li>
-          <p className="text-white">お問い合わせ</p>
-        </li>
+      <div className="flex justify-center flex-col">
+        <h2 className="text-white">車のあいみつ</h2>
         <WhiteButton text="お問い合わせ" />
-      </ul>
+      </div>
     </nav>
   );
 };
