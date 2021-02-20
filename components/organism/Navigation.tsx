8fa148505f9dd405ec68101118aaf976479b230d
kai815 <h.kaizuka815@gmail.com>
2021-02-20 19:34:04 +0900

    中央よせ

--- components/organism/Navigation.tsx
+++ components/organism/Navigation.tsx
@@ -7,17 +7,19 @@ const Navigation: FC = () => {
       <div className="absolute top-2.5 right-6">
         <span className="text-white inline-block p-2">×</span>
       </div>
-      <ul>
-        <li>
-          <p className="text-white">TOP</p>
-        </li>
-        <li>
-          <p className="text-white">サービス</p>
-        </li>
-        <li>
-          <p className="text-white">お問い合わせ</p>
-        </li>
-      </ul>
+      <div className="flex flex-col items-center">
+        <ul>
+          <li>
+            <p className="text-white">TOP</p>
+          </li>
+          <li>
+            <p className="text-white">サービス</p>
+          </li>
+          <li>
+            <p className="text-white">お問い合わせ</p>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
