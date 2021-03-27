0334ba206b6d2e39b55c73f7a9906fde7cbefc92
kai815 <h.kaizuka815@gmail.com>
2021-03-27 14:14:27 +0900

    Headinのstoryを分割

---
+++ components/atoms/Heading/UnderlineHeading.stories.tsx
@@ -0,0 +1,8 @@
+import React, { VFC } from "react";
+import UnderLine from "./UnderlineHeading";
+
+export default {
+  title: "Heading",
+};
+
+export const UnderLineHeader: VFC = () => <UnderLine text="ヘッダーテスト" />;
